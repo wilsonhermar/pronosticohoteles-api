5a7bf58b12693e38ca347b210f5507fbb2e46fa6
Wilson hernandez martinez <wilsonhermar@gmail.com>
2020-12-14 22:25:06 -0500

    Agregar permisos del api

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8080", "http://localhost:8000", "http://localhost:8082",
+    "http://localhost", "http://localhost:8080", "http://localhost:8000", 
+    "http://localhost:8082", "https://pronosticoshoteles-app.herokuapp.com",
 ]
 
 app.add_middleware(
